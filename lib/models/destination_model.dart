6c334246f86001b9910b7f02b019f09c4423fc5d
William Pettersen <williamkpettersen@outlook.com>
2024-10-06 23:33:41 +1100

    some models created, in progress

---
+++ lib/models/destination_model.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_travel_ui/models/activites';
+
+class Destination {
+  String imageUrl;
+  String city;
+  String country;
+  String description;
+  List<Activity> activities;
+
+  Destination({
+    this.imageUrl
+    this.city 
+    this.country 
+    this.description 
+    this.activities
+  });
+}
+
+List<Activity> activities = [
+  
+]
